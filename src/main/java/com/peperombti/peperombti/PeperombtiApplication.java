47cc1baeda55d90afbab7a85983a26bc34becb57
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-06 22:34:43 +0900

    init: initial commit

---
+++ src/main/java/com/peperombti/peperombti/PeperombtiApplication.java
@@ -0,0 +1,13 @@
+package com.peperombti.peperombti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeperombtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeperombtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peperombti/peperombti/PeperombtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peperombti.peperombti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeperombtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
