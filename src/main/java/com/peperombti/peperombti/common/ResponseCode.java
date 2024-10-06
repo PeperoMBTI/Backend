d9e86d274b760cde4ba943d4f4236fca1fe4fd07
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-06 22:49:12 +0900

    feat: Add ResponseCode, ResponseMessage

---
+++ src/main/java/com/peperombti/peperombti/common/ResponseCode.java
@@ -0,0 +1,11 @@
+package com.peperombti.peperombti.common;
+
+public interface ResponseCode {
+    //HTTP 200
+    String SUCCESS = "SU";
+
+    //HTTP 400
+
+    //HTTP 500
+    String DATABASE_ERROR = "DBE";
+}
---
+++ src/main/java/com/peperombti/peperombti/common/ResponseMessage.java
@@ -0,0 +1,11 @@
+package com.peperombti.peperombti.common;
+
+public interface ResponseMessage {
+    //HTTP 200
+    String SUCCESS = "success";
+
+    //HTTP 400
+
+    //HTTP 500
+    String DATABASE_ERROR = "database error";
+}
