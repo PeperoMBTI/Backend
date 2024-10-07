73e3005dfe7354b97844e887ada85e5aa0df80ee
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-08 00:37:24 +0900

    feat: Add QuestionRepository(#1)

--- src/main/java/com/peperombti/peperombti/repository/QuestionRepository.java
+++ src/main/java/com/peperombti/peperombti/repository/QuestionRepository.java
@@ -2,7 +2,9 @@ package com.peperombti.peperombti.repository;
 
 import com.peperombti.peperombti.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 }
