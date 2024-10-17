cf3b5cd7c8ca72b72293f61c09ee210aa14bb8d5
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-17 22:57:35 +0900

    feat: Add result request dto(#1)

---
+++ src/main/java/com/peperombti/peperombti/dto/request/ResultRequestDto.java
@@ -0,0 +1,19 @@
+package com.peperombti.peperombti.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResultRequestDto {
+    private int e;
+    private int i;
+    private int s;
+    private int n;
+    private int t;
+    private int f;
+    private int j;
+    private int p;
+}
