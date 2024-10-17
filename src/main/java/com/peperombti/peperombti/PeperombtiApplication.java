a1476d3f75a9dbc7e96030ed164156673db1cd8a
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-17 22:59:13 +0900

    feat: Modify PeperombtiApplication(#1)

--- src/main/java/com/peperombti/peperombti/PeperombtiApplication.java
+++ src/main/java/com/peperombti/peperombti/PeperombtiApplication.java
@@ -2,8 +2,14 @@ package com.peperombti.peperombti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
+@EnableAsync
 public class PeperombtiApplication {
 
 	public static void main(String[] args) {
