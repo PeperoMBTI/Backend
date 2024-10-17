5b2c0d2c2aeaaf50e53fc3cf09f0d31eedcd7e59
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-17 23:08:11 +0900

    feat: Add result calculation API(#1)

--- src/main/java/com/peperombti/peperombti/controller/MbtiController.java
+++ src/main/java/com/peperombti/peperombti/controller/MbtiController.java
@@ -1,11 +1,17 @@
 package com.peperombti.peperombti.controller;
 
+import com.peperombti.peperombti.dto.request.ResultRequestDto;
 import com.peperombti.peperombti.dto.response.QuestionResponseDto;
+import com.peperombti.peperombti.dto.response.ResultResponseDto;
 import com.peperombti.peperombti.service.MbtiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +21,12 @@ public class MbtiController {
     @GetMapping("/question")
     public ResponseEntity<? super QuestionResponseDto> getQuestion() {
         ResponseEntity<? super QuestionResponseDto> response = mbtiService.getQuestion();
+        return response;
+    }
+
+    @PostMapping("/result")
+    public CompletableFuture<ResponseEntity<? super ResultResponseDto>> calculateMbtiResult(@RequestBody ResultRequestDto dto) {
+        CompletableFuture<ResponseEntity<? super ResultResponseDto>> response = mbtiService.calculateResult(dto);
         return response;
     }
 }
--- src/main/java/com/peperombti/peperombti/service/MbtiService.java
+++ src/main/java/com/peperombti/peperombti/service/MbtiService.java
@@ -1,21 +1,38 @@
 package com.peperombti.peperombti.service;
 
+import com.peperombti.peperombti.domain.ParticipantCount;
 import com.peperombti.peperombti.domain.Question;
-import com.peperombti.peperombti.dto.response.OptionDto;
+import com.peperombti.peperombti.dto.OptionDto;
+import com.peperombti.peperombti.dto.request.ResultRequestDto;
 import com.peperombti.peperombti.dto.response.QuestionResponseDto;
+import com.peperombti.peperombti.dto.response.ResultResponseDto;
+import com.peperombti.peperombti.repository.ParticipantCountRepository;
 import com.peperombti.peperombti.repository.QuestionRepository;
+import com.peperombti.peperombti.repository.ResultRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 public class MbtiService {
 
     private final QuestionRepository questionRepository;
+    private final ResultRepository resultRepository;
+    private final ParticipantCountRepository participantCountRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String TOPIC = "mbti_results";
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     public ResponseEntity<? super QuestionResponseDto> getQuestion() {
         List<String> questionList = new ArrayList<>();
@@ -36,5 +53,92 @@ public class MbtiService {
             return QuestionResponseDto.databaseError();
         }
         return QuestionResponseDto.success(questionList, option1List, option2List);
+    }
+
+    @Async
+    public CompletableFuture<ResponseEntity<? super ResultResponseDto>> calculateResult(ResultRequestDto dto) {
+        String mbti;
+        try {
+            System.out.println("Received E: " + dto.getE());
+            System.out.println("Received I: " + dto.getI());
+            System.out.println("Received S: " + dto.getS());
+            System.out.println("Received N: " + dto.getN());
+            System.out.println("Received T: " + dto.getT());
+            System.out.println("Received F: " + dto.getF());
+            System.out.println("Received J: " + dto.getJ());
+            System.out.println("Received P: " + dto.getP());
+            mbti = calcMbti(dto.getE(), dto.getI(), dto.getS(), dto.getN(), dto.getT(), dto.getF(), dto.getJ(), dto.getP());
+
+            if (mbti.length() != 4)
+                return CompletableFuture.completedFuture(ResultResponseDto.invalidInput());
+
+            cacheParticipantInRedis(mbti);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return CompletableFuture.completedFuture(ResultResponseDto.databaseError());
+        }
+        return CompletableFuture.completedFuture(ResultResponseDto.success(mbti));
+    }
+
+    @Async
+    public void cacheParticipantInRedis(String mbti) {
+        try {
+            //참여자 수 저장
+            redisTemplate.opsForValue().increment("participant");
+
+            //mbti 결과 저장
+            kafkaTemplate.send(TOPIC, mbti);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String calcMbti(int e, int i, int s, int n, int t, int f, int j, int p) {
+        String result = "";
+        if (e > i)
+            result += "E";
+        else if (e < i)
+            result += "I";
+
+        if (s > n)
+            result += "S";
+        else if (s < n)
+            result += "N";
+
+        if (t > f)
+            result += "T";
+        else if (t < f)
+            result += "F";
+
+        if (j > p)
+            result += "J";
+        else if (j < p)
+            result += "P";
+
+        System.out.println(result);
+        return result;
+    }
+
+    @Scheduled(cron = "0 */3 * * * *")
+    public void syncParticipantCount() {
+        String participantCache = redisTemplate.opsForValue().get("participant");
+        Long participantCount = Long.parseLong(participantCache);
+
+        Optional<ParticipantCount> optionalParticipantCount = participantCountRepository.findById(1L);
+        if (optionalParticipantCount.isEmpty()) {
+            ParticipantCount dbParticipantCount = new ParticipantCount();
+            dbParticipantCount.setId(1L);
+            dbParticipantCount.setParticipant(participantCount);
+            dbParticipantCount.setCreatedAt(LocalDateTime.now());
+            dbParticipantCount.setUpdatedAt(LocalDateTime.now());
+            participantCountRepository.save(dbParticipantCount);
+        } else {
+            ParticipantCount dbParticipantCount = optionalParticipantCount.get();
+            dbParticipantCount.setParticipant(participantCount);
+            dbParticipantCount.setUpdatedAt(LocalDateTime.now());
+            participantCountRepository.save(dbParticipantCount);
+        }
     }
 }
